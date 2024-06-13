ee16b0231478be342934192a761bc71c807d15c7
oliviawissig <oliviawissig@gmail.com>
2024-06-13 16:42:41 -0700

    adding missing <li> tags

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -13,7 +13,7 @@ const NavBar = () => {
         <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
             <Link href="/"><FaBug /></Link>
             <ul className="flex space-x-6">
-                {links.map(link => <Link key={link.href} className="text-zinc-500 hover:text-zinc-800 transition-colors" href={link.href}>{link.label}</Link>)}
+                {links.map(link => <li><Link key={link.href} className="text-zinc-500 hover:text-zinc-800 transition-colors" href={link.href}>{link.label}</Link></li>)}
             </ul>
         </nav>
     )
